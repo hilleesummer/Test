0fc02c1a2346879af97e330faf18253b9bfb7b2a
hilleesummer <hilleesummer@gmail.com>
2022-07-10 19:39:56 +0900

    Create MyClient.java

---
+++ new/http_client/MyClient.java
@@ -0,0 +1,8 @@
+public class MyClient {
+  public static void main(String[] args) throws Exception {
+    HttpClient httpClient = new HttpClient();
+    httpClient.start();
+    ContentResponse contentRes = httpClient.newRequest("http://127.0.0.1:8080/mypath").method(HttpMethod.GET).send();
+    System.out.println(contentRes.getContentAsString());
+  }
+}
