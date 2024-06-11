c1ac7511035c504b5a8654e716402db5a415899c
hilleesummer <hilleesummer@gmail.com>
2024-06-11 23:46:04 +0900

    Create DeviceInfo.java

---
+++ new/http_client_server/DeviceInfo.java
@@ -0,0 +1,30 @@
+
+public class DeviceInfo {
+	private String device;
+	private String hostname;
+	private Integer port;
+	
+	public String getDevice() {
+		return device;
+	}
+	
+	public void setDevice(String device) {
+		this.device = device;
+	}
+	
+	public String getHostname() {
+		return hostname;
+	}
+	
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	
+	public Integer getPort() {
+		return port;
+	}
+	
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+}
