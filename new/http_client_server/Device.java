559448e983198d57c2f5bc1cb7087f39f1503021
hilleesummer <hilleesummer@gmail.com>
2024-06-11 23:45:43 +0900

    Create Device.java

---
+++ new/http_client_server/Device.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Device {
+	private List<DeviceInfo> deviceInfo;
+	
+	public List<DeviceInfo> getDeviceInfo(){
+		return deviceInfo;
+	}
+	
+	public void setDeviceInfo(List<DeviceInfo> deviceInfo) {
+		this.deviceInfo = deviceInfo;
+	}
+}
