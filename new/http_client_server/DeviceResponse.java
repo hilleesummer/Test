21547999dacc4262617d2466d584438a801a16ac
hilleesummer <hilleesummer@gmail.com>
2024-06-11 23:46:30 +0900

    Create DeviceResponse.java

---
+++ new/http_client_server/DeviceResponse.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class DeviceResponse {
+	private List<String> result;
+	
+	public List<String> getResult(){
+		return result;
+	}
+	
+	public void setResult(List<String> result) {
+		this.result = result;
+	}
+}
